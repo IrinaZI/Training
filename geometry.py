636ec1a55bd5c34ed6405177a369cc509b35ded6
IrinaZI <30446195+IrinaZI@users.noreply.github.com>
2017-10-21 00:43:40 +0400

    Циклы

--- geometry.py
+++ geometry.py
@@ -1,15 +1,11 @@
 
 from geom2d import *
 
-#a = Point(0,0)
-#b = Point(3,4)
-#print(a.distance(b))
-#print (a==b)
-#print (a == Point(0,0))
-
-l1 = [Point(3, 1), Point(0, 0), Point (1, 2)]
+l = [Point(i, i*i)for i in range (-5, 6)]
 
 
-l2 = sorted(l1, key= lambda p: p.distance(Point(0,0)))
-print(l1)
-print(l2)
+l2 = [Point(el.x, -el.y)for el in l ]
+
+
+print (l)
+print (l2)
